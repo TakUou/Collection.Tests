94bb897ba87d2946d3c2c71de3009a9e51fcd698
TakUou <Tokimeki56@mail.ru>
2023-10-12 15:01:11 +0300

    fff

---
+++ Collection.Tests/CollectionTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+using TestMethods;
+
+namespace Collection.Tests
+{
+    [TestClass]
+    public class CollectionTests
+    {
+
+        static List<string> list;
+        static List<string> list2;
+        static List<string> list3;
+        static List<string> list4;
+
+        TestMethodsTests TMT = new TestMethodsTests();
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            list = new List<string>
+            {
+                "3","2"
+            };
+            list2 = new List<string>
+            {
+               "2"
+            };
+            list3 = new List<string>
+            {
+               "корней нет"
+            };
+            list4 = new List<string>
+            {
+                "15f", "0.5"
+            };
+        }
+
+        [TestMethod]
+        public void TestMethod_FindTwoRoots_returnTwoRoots()
+        {
+            CollectionAssert.AreEquivalent(list, TMT.TestMethod(1,-5,6));
+        }
+
+        [TestMethod]
+        public void TestMethod_FindRoot_returnRoot()
+        {
+            CollectionAssert.AreEqual(list2, TMT.TestMethod(1,-4, 4));
+        }
+
+        [TestMethod]
+        public void TestMethod_FindNoRoots_returnNoRoots()
+        {
+            CollectionAssert.AreEqual(list3, TMT.TestMethod(2, 3, 7));
+        }
+
+        [TestMethod]
+        public void CalculatePercentage_FindPercentageFromNumber_PersentageFromNumber()
+        {
+            double number = 50.0f, percentage = 10.0f;
+            double[] actual = new double[] { 5.0f };
+            var res = TMT.CalculatePercentage(number, percentage);
+            double[] expected = new double[] { res };
+            CollectionAssert.AreEqual(expected, actual, $"expected - {expected[0]}, actual - {actual[0]}");
+        }
+    }
+}
---
+++ TestMethods/TestMethodsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMethods
+{
+    public class TestMethodsTests
+    {
+        public List<string> TestMethod(double a, double b, double c)
+        {
+            List<string> ret = new List<string>();
+            double x1, x2;
+            var discriminant = Math.Pow(b, 2) - 4 * a * c;
+            if (discriminant < 0)
+            {
+                ret.Add("корней нет");
+                return ret;
+            }
+            else
+            {
+                if (discriminant == 0) //квадратное уравнение имеет два одинаковых корня
+                {
+                    x1 = -b / (2 * a);
+                    ret.Add(x1.ToString());
+                    return ret;
+                    
+                }
+                else //уравнение имеет два разных корня
+                {
+                    x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                    ret.Add(x2.ToString());
+                    ret.Add(x1.ToString());
+                    return ret;
+                }
+            }
+        }
+        public double CalculatePercentage(double number, double percentage)
+        {
+            return number * percentage / 100;
+        }
+    }
+}
